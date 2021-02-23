a4e05b6656f3eaa289ae242c03c6eea093349db7
Alina Muliak <alina.muliak@ucu.edu.ua>
2021-02-23 15:11:36 +0200

    Update the documentation of this file

--- json_helper.py
+++ json_helper.py
@@ -1,24 +1,7 @@
 """
-Модуль обов'язково повинен містити функцію для забезпечення доступу
-до різних частин json об'єкта. Наприклад, попросити користувача
-ввести ключ словника, значення якого він хоче переглянути.
-Проект повинен бути розміщений як окремий репозиторій на GitHub. 
-
-Припустимо, API повернуло нам певний об'єкт, певні значення якого
-я як користувач хочу переглянути. При розробці модуля ви можете припустити,
-що користувач знає, які поля є в об'єкті, але користувачу буде приємніше,
-якщо йому підкажуть набір полів, серед яких він може вибрати бажане.
-У відповідь на введений користувачем ключ, потрібно показати відповідне йому значення.
-Але є декілька випадків, в яких програма може повести себе по-іншому:
-♦ якщо значення, яке відповідає ключу теж є об'єктом -
-    в такому випадку можна відобразити весь об'єкт повністю,
-    або повідомити користувача, що це теж об'єкт, і відобразити доступні ключі
-♦ якщо значення є списком - можна просто показати весь список,
-    або ж повідомити що це список, спитати, який за номером елемент списку відобразити...
-    варіантів реалізації може бути безліч
-Важливо розуміти, що немає єдиного правильного способу,
-вони обмежуються лише вашою фантазією, важливо, щоб зберігалась головна умова -
-навігація по json-об'єкту (бажано, щоб вона була ще й зручною).
+This module contains a functions that help to provide
+an access to different parts of the json object
+(navigation through the json-object).
 """
 
 from time import sleep
@@ -48,6 +31,11 @@ def show_info_user() -> None:
     """
     The main function of json_helper module.
     Help user to navigate through the certain JSON file.
+
+    ♦ if the value corresponding to the key is also an object -
+     in this case, inform the user that it is also an object and display the available keys
+    ♦ if the value is a list - report that it is a list, ask whether user wants to see it all or
+    which number of the list item to display
     """
     try:
         path_to_file = input(slow("please enter the path to your JSON file: "))
@@ -56,8 +44,7 @@ def show_info_user() -> None:
 
         while True:
             if isinstance(next_step, dict):
-                slow(
-                    f"\nthis object is a dict.\nit has these keys:\n\n")
+                slow("\nthis object is a dict.\nit has these keys:\n\n")
                 for this_key in next_step.keys():
                     slow(this_key + '    ')
                 next_index = input(slow("\nwhich one do you want to see? "))
@@ -75,7 +62,7 @@ def show_info_user() -> None:
                 slow(f"\nthis is the list, it has {len_of_list} elements\n")
                 answer = input(slow("do you want to see them all? (y/n): "))
 
-                while answer != 'n' and answer != 'y':
+                while answer not in ('n', 'y'):
                     answer = input(slow("please, type 'y' or 'n': "))
 
                 if answer.lower() == 'y':
@@ -83,7 +70,8 @@ def show_info_user() -> None:
                     break
 
                 next_index = int(input(slow(
-                    f"\nokay, then, please type the index (0-{len_of_list}) of the certain element you want to see: ")))
+                    f"\nokay, then, please type the index (0-{len_of_list}) \
+of the certain element you want to see: ")))
                 if next_index not in range(0, len_of_list):
                     slow("\ncheck your input and type a number from the range! ")
                     continue
@@ -96,7 +84,8 @@ def show_info_user() -> None:
                 break
 
     except FileNotFoundError:
-        slow("check your input because there is no such file according to your path and try again:)")
+        slow("check your input because there is no such file\
+according to your path and try again:*")
 
 
 if __name__ == "__main__":
